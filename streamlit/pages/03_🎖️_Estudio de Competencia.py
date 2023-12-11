781753223b79e2a91f069e755284e6058956d708
JoaquiJullier <JoaquiJullier@gmail.com>
2023-12-11 10:56:16 -0300

    Streamlit modifications

--- streamlit/pages/03_🎖️_Estudio de Competencia.py
+++ streamlit/pages/03_🎖️_Estudio de Competencia.py
@@ -22,3 +22,33 @@ st.set_page_config(
     page_title="Tu Aplicación",
     page_icon=":chart_with_upwards_trend:"
 )
+
+selected_state = st.sidebar.selectbox('State', ["California", "Florida"])
+
+
+california = usa_states[usa_states["state_id"] == "CA"]
+
+cities_map = px.scatter_geo(
+    clients,
+    lat="latitude",
+    lon="longitude",
+    #size="hotel_count",
+    color_discrete_sequence=["blue"],
+    title="Geographic distribution:",
+    width=1000,
+    height=900,
+    scope="usa")
+cities_map.update_traces(marker=dict(line=dict(width=0)), opacity=0.7)  # Ajustar la opacidad
+cities_map.update_layout(title_x=0.7)
+
+
+col1, col2 = st.columns((2, 5))
+
+with col1:
+    ...
+    #st.plotly_chart(bar_chart, use_container_width=True)
+    #st.plotly_chart(top_cities, use_container_width=True)
+    #st.plotly_chart(scatter_plot, use_container_width=True)
+    
+with col2:
+    st.plotly_chart(cities_map, use_container_width=True)

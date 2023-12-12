d33242cee2fe82706c2c9855be2b7414c39a6780
Leonardo Costa <105021198+LeonardoCostaH@users.noreply.github.com>
2023-12-12 01:05:19 -0300

    Update 03_🎖️_Estudio de Competencia.py
    actualzación de conexión para postgresql

--- pages/03_🎖️_Estudio de Competencia.py
+++ pages/03_🎖️_Estudio de Competencia.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from connect import cursor, conn
+from connect import cursor, conn, obtener_datos
 import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
@@ -11,11 +11,11 @@ import plotly.graph_objects as go
 
 warnings.filterwarnings("ignore")
 
-usa_states = pd.read_csv("../files/data/usa_states.csv")
-usa_cities = pd.read_csv("../files/data/usa_cities.csv")
-clients = pd.read_csv("../files/data/usa_clients.csv", index_col=0)
-california_hotels = pd.read_csv("../files/data/booking/california_hotels.csv", index_col=0)
-matrix = pd.read_csv("../files/data/booking/california_hotels_similarity_matrix.csv", index_col=0)
+usa_states = obtener_datos(cursor, "usa_states", "*")
+usa_cities = obtener_datos(cursor, "usa_cities", "*")
+clients = obtener_datos(cursor, "usa_clients", "*")
+california_hotels = obtener_datos(cursor, "booking_hotels", "*")
+matrix = obtener_datos(cursor, "california_hotels_similarity_matrix", "*")
 
 
 st.set_page_config(
